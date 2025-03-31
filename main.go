3981d9b71cc02ad5127ecdf2c479443166ef8b17
Aman Verma <amanraoverma@gmail.com>
2025-03-31 01:28:02 -0500

    Remove specific options from usage line.

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func mainWithErr() error {
 
 	if *helpRequested || filename == "" {
 		pflag.CommandLine.SetOutput(os.Stdout)
-		fmt.Println("Usage: ziphttp [-bh] <PATH TO ZIP FILE>")
+		fmt.Println("Usage: ziphttp [OPTIONS] <PATH TO ZIP FILE>")
 		fmt.Println()
 		pflag.PrintDefaults()
 		return nil
