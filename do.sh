48733f228f2d7d1a46dddd7c28fd478f8d5d14a0
Aman Verma <amanraoverma@gmail.com>
2025-04-14 16:57:32 -0500

    do.sh: housekeeping.

--- do.sh
+++ do.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -eu
 
-args="-ldflags "-s" -trimpath -tags osusergo,netgo"
+args="-ldflags -s -trimpath -tags osusergo,netgo"
 
 GOAMD64=v3 go build $args -o target/ .
 # GOOS=windows go build $args -o target/ .
