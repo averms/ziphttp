2617141943b225d3dba44385a3c92f9fd244245e
Aman Verma <amanraoverma@gmail.com>
2025-04-20 02:30:05 -0500

    do.sh: improve cross-compiling commands.

--- do.sh
+++ do.sh
@@ -4,5 +4,6 @@ set -eu
 args="-ldflags -s -trimpath -tags osusergo,netgo"
 
 GOAMD64=v3 go build $args -o target/ .
-# GOOS=windows go build $args -o target/ .
-# GOOS=darwin GOARCH=arm64 $args -o target/ .
+# GOOS=windows GOARCH=amd64 GOAMD64=v3 go build $args -o target/ .
+# GOOS=linux GOARCH=arm64 go build $args -o target/ .
+# GOOS=darwin GOARCH=arm64 go build $args -o target/ .
